f345f992c879dde59b86551af7173e311395bfa7
Dikman Satya Nugraha <dikmansn@gmail.com>
2023-06-04 00:43:21 +0700

    commit 1

---
+++ add.php
@@ -0,0 +1,52 @@
+<html>
+
+<head>
+    <title>Tambah Dosen</title>
+</head>
+
+<body>
+    <a href="index.php">Kembali ke Home</a>
+    <br /><br />
+
+    <form action="add.php" method="post" name="form1">
+        <table width="25%" border="0">
+            <tr>
+                <td>Nama</td>
+                <td><input type="text" name="nama"></td>
+            </tr>
+            <tr>
+                <td>NIDN</td>
+                <td><input type="text" name="nidn"></td>
+            </tr>
+            <tr>
+                <td>Jenjang Pendidikan</td>
+                <td><input type="text" name="jenjang_pendidikan"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" name="Submit" value="Tambah"></td>
+            </tr>
+        </table>
+    </form>
+
+    <?php
+
+    // Check If form submitted, insert form data into users table.
+    if (isset($_POST['Submit'])) {
+        $nama = $_POST['nama'];
+        $nidn = $_POST['nidn'];
+        $jenjang_pendidikan = $_POST['jenjang_pendidikan'];
+
+        // include database connection file
+        include_once("config.php");
+
+        // Insert user data into table
+        $result = mysqli_query($mysqli, "INSERT INTO dosen(nama,nidn,jenjang_pendidikan) VALUES('$nama','$nidn','$jenjang_pendidikan')");
+
+        // Show message when user added
+        echo "Data Dosen berhasil ditambahkan. <a href='index.php'>Lihat Data Dosen</a>";
+    }
+    ?>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+// Create database connection using config file
+include_once("config.php");
+
+// Fetch all users data from database
+$result = mysqli_query($mysqli, "SELECT * FROM dosen ORDER BY id DESC");
+?>
+
+<html>
+
+<head>
+    <title>Data Dosen</title>
+</head>
+
+<body>
+    <a href="add.php">Tambah Dosen</a><br /><br />
+
+    <table width='80%' border=1>
+
+        <tr>
+            <th>Nama</th>
+            <th>NIDN</th>
+            <th>JENJANG PENDIDIKAN (S2 ATAU S3)</th>
+            <th>Update</th>
+        </tr>
+        <?php
+        while ($user_data = mysqli_fetch_array($result)) {
+            echo "<tr>";
+            echo "<td>" . $user_data['nama'] . "</td>";
+            echo "<td>" . $user_data['nidn'] . "</td>";
+            echo "<td>" . $user_data['jenjang_pendidikan'] . "</td>";
+            echo "<td><a href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Hapus</a></td></tr>";
+        }
+        ?>
+    </table>
+</body>
+
+</html>
